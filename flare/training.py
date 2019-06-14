69c939b3d5a19c19bbfc61de59328f97d53e688d
Abhai Kollara <abhai.dilip@gmail.com>
2019-06-14 17:23:28 +0530

    Update skelton

--- flare/training.py
+++ flare/training.py
@@ -3,7 +3,7 @@ import torch
 class Trainer(object):
 
     def __init__(self):
-        print("Yaay, it works")
+        pass
     
     def train(self):
         pass
@@ -12,4 +12,22 @@ class Trainer(object):
         pass
     
     def predict(self):
+        pass
+
+    def train_generator(self):
+        pass
+    
+    def evaluate_generator(self):
+        pass
+    
+    def predict_generator(self):
+        pass
+
+    def train_batch(self):
+        pass
+    
+    def evaluate_batch(self):
+        pass
+    
+    def predict_batch(self):
         pass
