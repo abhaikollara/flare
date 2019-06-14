79d6769a95645abcf0164eb15a2df5e6c01f5189
Abhai Kollara <abhai.dilip@gmail.com>
2019-06-14 18:16:30 +0530

    Basic batch train, test, predict

--- flare/training.py
+++ flare/training.py
@@ -1,9 +1,13 @@
 import torch
 
+
 class Trainer(object):
 
-    def __init__(self):
-        pass
+    def __init__(self, model, loss, optimizer, device="cpu"):
+        self.model = model
+        self.loss_fn = loss
+        self.optimizer = optimizer
+        self.device = torch.device(device)
     
     def train(self):
         pass
@@ -23,11 +27,31 @@ class Trainer(object):
     def predict_generator(self):
         pass
 
-    def train_batch(self):
-        pass
+    def train_batch(self, inputs, targets):
+        inputs = inputs.to(self.device)
+        targets = targets.to(self.device)
+
+        self.optimizer.zero_grad()
+        output = self.model(inputs)
+        loss = self.loss_fn(output, targets)
+        loss.backward()
+        self.optimizer.step()
+
+        return loss
     
-    def evaluate_batch(self):
-        pass
+    def evaluate_batch(self, inputs, targets):
+        inputs = inputs.to(self.device)
+        targets = targets.to(self.device)
+
+        output = self.model(inputs)
+        loss = self.loss_fn(output, targets)
+
+        return loss
     
-    def predict_batch(self):
-        pass
+    def predict_batch(self, inputs):
+        inputs = inputs.to(self.device)
+
+        output = self.model(inputs)
+
+        return output
+
