a3c489701b76cff1a751445084e0dba279d875d0
Abhai Kollara <abhai.dilip@gmail.com>
2019-06-14 17:13:11 +0530

    Setup file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(name="flare",
+      version='0.0.1',
+      description='Training helper for PyTorch',
+      author="Abhai Kollara Dilip",
+      author_email="abhai.dilip@gmail.com",
+      packages=find_packages())
