70e15eef830a6158a05a0ab120435116c003f4c7
Abhai Kollara <abhai.dilip@gmail.com>
2019-06-14 17:11:59 +0530

    Basic structure

---
+++ flare/training.py
@@ -0,0 +1,15 @@
+import torch
+
+class Trainer(object):
+
+    def __init__(self):
+        print("Yaay, it works")
+    
+    def train(self):
+        pass
+    
+    def evaluate(self):
+        pass
+    
+    def predict(self):
+        pass
