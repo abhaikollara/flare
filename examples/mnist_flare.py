2e7c5e1cd33322bb6b83882072c4f0cc420ae3e7
Abhai Kollara <abhai.dilip@gmail.com>
2019-06-19 17:40:16 +0530

    Add mnist example

--- examples/mnist_flare.py
+++ examples/mnist_flare.py
@@ -3,11 +3,11 @@ from torch import nn
 from torch.optim import Adam
 import torch.nn.functional as F
 import numpy as np
+import os
 
 import time
 import flare
 from flare import Trainer
-from flare.data import FlareDataset
 
 class Net(nn.Module):
     def __init__(self):
@@ -27,19 +27,24 @@ class Net(nn.Module):
         x = self.fc2(x)
     
         return F.log_softmax(x, dim=1)
-    
-train_data = np.load("./data/mnist_train.npz")
+
+train_data = np.load(os.path.abspath("./data/mnist_train.npz"))
 train_x, train_y = train_data['X_train'], train_data['Y_train']
 
 train_x = np.expand_dims(train_x, 1).astype('float32')
 train_y = train_y.reshape(-1)
 
-test_data = np.load("./data/mnist_test.npz")
+test_data = np.load(os.path.abspath("./data/mnist_test.npz"))
 test_x, test_y = test_data['X_test'], test_data['Y_test']
 
 test_x = np.expand_dims(test_x, 1).astype('float32')
-test_y = train_y.reshape(-1)
+test_y = test_y.reshape(-1)
+
+
+train_x /= 255.0
+test_x /= 255.0
 
 model = Net()
 trainer = Trainer(model, F.nll_loss, Adam(model.parameters()))
 trainer.train(train_x, train_y, batch_size=64)
+trainer.evaluate(test_x, test_y)
